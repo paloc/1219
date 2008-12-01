9f6d32a3d43e1cdbf68edbec59df55bbec6476bb
paloc <palo@tamizdat.org>
2008-12-01 06:24:06 -0600

    Changed About Us to single column layout. Content added.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -3,5 +3,8 @@ class RootController < ApplicationController
   def faq
   	render :layout => 'application_one_column'
   end
-  
+
+  def about
+  	render :layout => 'application_one_column'
+  end
 end
