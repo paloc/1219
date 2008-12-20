732b9b9b8ebedbe01262fa7d9956647b02aa2a00
paloc <palo@tamizdat.org>
2008-12-20 17:45:23 -0600

    loop staff pics

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -6,7 +6,16 @@ class LibraryController <  ApplicationController
     unless staff_pics.nil?
       @minutes = staff_pics.minutes
     end
-
+    
+    if params[:id].to_i == @minutes.length
+      indx = 0
+	else
+	  indx = params[:id].to_i
+	end
+    
+    @minute = @minutes[indx]
+	@nextPicIndex = indx + 1
+	
     render :layout => 'application_one_column'
   end
 	
